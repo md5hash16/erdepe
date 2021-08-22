49d3cfbf0af2453848f3b800cf565512aae47bfe
md5hash16 <64632094+md5hash16@users.noreply.github.com>
2021-08-22 15:31:21 +0800

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -2,10 +2,10 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-curl -O https://raw.githubusercontent.com/MR-THARUWA/WindowsRDP3/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/MR-THARUWA/WindowsRDP3/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\EVKey Vietnamese Keyboard.exe" https://raw.githubusercontent.com/MR-THARUWA/WindowsRDP3/main/EVKey64.exe > out.txt 2>&1
-net user administrator Tharusha123. /add >nul
+curl -O https://raw.githubusercontent.com/md5hash16/erdepe/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/md5hash16/erdepe/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\EVKey Vietnamese Keyboard.exe" https://raw.githubusercontent.com/md5hash16/erdepe/main/EVKey64.exe > out.txt 2>&1
+net user administrator Hideyoshi1. /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 diskperf -Y >nul
@@ -17,6 +17,6 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-r
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels "
 echo User: Administrator
-echo Pass: Tharusha123.
+echo Pass: Hideyoshi1.
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 ping -n 10 127.0.0.1 >nul
